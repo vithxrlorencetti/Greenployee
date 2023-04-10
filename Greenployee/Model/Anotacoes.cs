ba65df8a110150f0d718ff35e3342b7a52a1f428
Gustavo <gustavo.koslovski2001@gmail.com>
2023-04-09 22:47:03 -0300

    Class Pessoa,Anotacoes

---
+++ Greenployee/Model/Anotacoes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Greenployee.Model
+{
+    public class Anotacoes:BaseClass
+    {
+        public DateTime dtAnotacao { get; set; }
+        public DateTime dsMensagem { get; set; }
+
+        [ForeignKey("Pessoa")]
+        public int idPessoa { get; set; }   
+
+    }
+}
---
+++ Greenployee/Model/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greenployee.Model
+{
+    public class Pessoa: BaseClass
+    {
+        [StringLength(60)]
+        public string nmPessoa { get; set; } = string.Empty;
+
+        [StringLength(11)]
+        public string nrCPF { get; set; } = string.Empty;
+
+        [StringLength(15)]
+        public string nrRG { get; set;} = string.Empty;
+
+        [StringLength(50)]
+        public string dsEmail { get; set; } = string.Empty;
+
+        [StringLength(20)]
+        public string nrTelefone { get; set; } = string.Empty;
+
+        [StringLength(15)]
+        public string flSituacao { get; set; } = string.Empty;
+
+        [StringLength(15)]
+        public string nrPIS { get; set; } = string.Empty;
+
+        public decimal flEntrega { get; set; }
+        public DateTime dtAdmissao { get; set; }
+        
+    }
+}
